4c9360035664ca2e8f203726f4e2fbebba2234fc
Esteve Julià <estevejuliamelis@gmail.com>
2016-04-17 00:28:39 +0200

    search refactor

--- src/Bridge/Symfony/FlowUIBundle/Resources/public/search.js
+++ src/Bridge/Symfony/FlowUIBundle/Resources/public/search.js
@@ -1,18 +1,22 @@
 (function ($, d3, flow) {
 
-    function init(optArray) {
-        optArray = optArray.sort();
+    function init(nodeIds) {
+        nodeIds = nodeIds.sort();
 
         $("#search-form").show();
 
         $("#search-input").autocomplete({
-            source: optArray
+            source: nodeIds
         });
 
+        setUpEvents();
+    }
+
+    function setUpEvents() {
         $(document).on('submit', '#search-form', function(e) {
             e.preventDefault();
-            var selectedVal = document.getElementById('search-input').value;
-            searchNode(selectedVal);
+            var inputValue = document.getElementById('search-input').value;
+            searchNode(inputValue);
         });
 
         $(document).on('click', '.validation-item', function() {
@@ -21,27 +25,34 @@
         });
     }
 
-    function searchNode(selectedVal) {
-        //find the node
-        var svg = d3.selectAll('.graph-container > svg');
-        var node = svg.selectAll(".node");
-        if (selectedVal == "none") {
-            node.style("stroke", "white").style("stroke-width", "1");
-        } else {
-            var selected = node.filter(function (d, i) {
-                return d.id != selectedVal;
-            });
-            selected.style("opacity", "0");
-            var link = svg.selectAll(".link")
-            link.style("opacity", "0");
-            d3.selectAll(".node, .link").transition()
-                .duration(2000)
-                .style("opacity", 1);
+    function searchNode(id) {
+        var selectedNode = document.getElementById(id);
 
-            $('html, body').animate({
-                scrollTop: $("#" + selectedVal).offset().top - (height/2)
-            }, 500);
+        if (selectedNode !== null) {
+            scrollToNode(selectedNode);
         }
+
+        temporaryFadeAllExcept("#" + id);
+    }
+
+    function temporaryFadeAllExcept(exceptionSelector) {
+        var svg = d3.selectAll('.graph-container > svg');
+        var nodes = svg.selectAll(".node:not(" + exceptionSelector + ")");
+        var links = svg.selectAll(".link");
+
+        nodes.style("opacity", "0");
+        links.style("opacity", "0");
+
+        d3.selectAll(".node, .link")
+            .transition()
+            .duration(2000)
+            .style("opacity", 1);
+    }
+
+    function scrollToNode(node) {
+        $('html, body').animate({
+            scrollTop: $(node).offset().top - (height/2)
+        }, 500);
     }
 
     flow.component.search = {

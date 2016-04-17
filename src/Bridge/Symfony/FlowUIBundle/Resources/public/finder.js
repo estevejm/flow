60af98abc2a7ec119e253d1d7c858315c10b2e41
Esteve Julià <estevejuliamelis@gmail.com>
2016-04-17 14:40:15 +0200

    Move temp fade to finder

--- src/Bridge/Symfony/FlowUIBundle/Resources/public/finder.js
+++ src/Bridge/Symfony/FlowUIBundle/Resources/public/finder.js
@@ -1,4 +1,4 @@
-(function (flow, $) {
+(function (flow, $, d3) {
 
     function find(nodeId) {
         var selectedNode = document.getElementById(nodeId);
@@ -7,7 +7,7 @@
             scrollToNode(selectedNode);
         }
 
-        flow.component.graph.temporaryFadeAllExcept(nodeId);
+        temporaryFadeAllExcept(nodeId);
     }
 
     function scrollToNode(node) {
@@ -16,8 +16,23 @@
         }, 500);
     }
 
+    function temporaryFadeAllExcept(nodeId) {
+        var nodes = d3.selectAll(".node:not(#" + nodeId + ")");
+        var labels = d3.selectAll(".label:not([data-for-node='" + nodeId + "'])");
+        var links = d3.selectAll(".link");
+
+        nodes.style("opacity", "0");
+        labels.style("opacity", "0");
+        links.style("opacity", "0");
+
+        d3.selectAll(".node, .link, .label")
+            .transition()
+            .duration(2000)
+            .style("opacity", 1);
+    }
+
     flow.component.finder = {
         find: find
     };
 
-}(flow, jQuery));
+}(flow, jQuery, d3));
--- src/Bridge/Symfony/FlowUIBundle/Resources/public/graph.js
+++ src/Bridge/Symfony/FlowUIBundle/Resources/public/graph.js
@@ -108,23 +108,4 @@
         return container;
     }
 
-    function temporaryFadeAllExcept(nodeId) {
-        var nodes = d3.selectAll(".node:not(#" + nodeId + ")");
-        var labels = d3.selectAll(".label:not([data-for-node='" + nodeId + "'])");
-        var links = d3.selectAll(".link");
-
-        nodes.style("opacity", "0");
-        labels.style("opacity", "0");
-        links.style("opacity", "0");
-
-        d3.selectAll(".node, .link, .label")
-            .transition()
-            .duration(2000)
-            .style("opacity", 1);
-    }
-
-    flow.component.graph = {
-        temporaryFadeAllExcept: temporaryFadeAllExcept
-    };
-
 }(flow, jQuery, d3));

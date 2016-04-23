6f198112ecd0869cab63e490e348a4f878f904de
Esteve Julià <estevejuliamelis@gmail.com>
2016-04-23 02:13:35 +0200

    Added integration test framework agnostic

--- tests/Integration/GetGraphTest.php
+++ tests/Integration/GetGraphTest.php
@@ -1,16 +1,50 @@
 <?php
 
-namespace EJM\Flow\Tests\Functional\Features;
+namespace EJM\Flow\Tests\Integration\Features;
 
+use EJM\Flow\Collector\MessagesToPublishCollector;
+use EJM\Flow\Mapper\D3\ForceLayoutMapper;
 use EJM\Flow\Network\Builder;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use EJM\Flow\Network\Builder\AssemblyStage\AddCommandsAndHandlers;
+use EJM\Flow\Network\Builder\AssemblyStage\AddEventsAndSubscribers;
+use EJM\Flow\Network\Builder\AssemblyStage\AddPublishedMessages;
+use EJM\Flow\Network\Splitter;
+use PHPUnit_Framework_TestCase;
 
-class GetGraphTest extends WebTestCase
+class GetGraphTest extends PHPUnit_Framework_TestCase
 {
-
     public function testAction()
     {
-        $expectedResponse = [
+        $mapperConfig = [
+            ForceLayoutMapper::MAP_HANDLERS => true,
+            ForceLayoutMapper::MAP_SUBSCRIBERS => true,
+        ];
+
+        $commandHandlerMap = [
+            'execute_command' => [
+                'id' => 'execute_command_handler',
+                'class' => 'EJM\\Flow\\Tests\\Functional\\Sandbox\\SimpleBus\\Command\\ExecuteCommandHandler',
+            ],
+            'execute_command_2' => [
+                'id' => 'execute_command_2_handler',
+                'class' => 'EJM\\Flow\\Tests\\Functional\\Sandbox\\SimpleBus\\Command\\ExecuteCommand2Handler',
+            ],
+        ];
+
+        $eventSubscribersMap = [
+            'command_executed' => [
+                [
+                    'id' => 'log_command_executed',
+                    'class' => 'EJM\\Flow\\Tests\\Functional\\Sandbox\\SimpleBus\\Subscriber\\LogCommandExecuted',
+                ],
+                [
+                    'id' => 'trigger_execute_command_2',
+                    'class' => 'EJM\\Flow\\Tests\\Functional\\Sandbox\\SimpleBus\\Subscriber\\TriggerExecuteCommand2',
+                ],
+            ],
+        ];
+
+        $expectedGraph = [
             [
                 'nodes' =>[
                     [
@@ -90,11 +124,27 @@ class GetGraphTest extends WebTestCase
                 ],
             ],
         ];
-        
-        $client = static::createClient();
-        $client->request('GET', '/flow/graphs');
-        $response = json_decode($client->getResponse()->getContent(), true);
 
-        $this->assertEquals($expectedResponse, $response);
+        $builder = new Builder();
+        $builder->withAssemblyStage(new AddCommandsAndHandlers($commandHandlerMap));
+        $builder->withAssemblyStage(new AddEventsAndSubscribers($eventSubscribersMap));
+        $builder->withAssemblyStage(new AddPublishedMessages(new MessagesToPublishCollector()));
+
+        $network = $builder->build();
+
+        $splitter = new Splitter();
+
+        $networks = $splitter->split($network);
+
+        $mapper = new ForceLayoutMapper($mapperConfig);
+
+        $graph = array_map(
+            function($network) use ($mapper) {
+                return $mapper->map($network);
+            },
+            $networks
+        );
+
+        $this->assertEquals($expectedGraph, json_decode(json_encode($graph), true));
     }
 }
